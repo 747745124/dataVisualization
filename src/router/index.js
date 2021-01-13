3832650b7fb0a96882f031dd2a229e9d242fec42
FRDlesmsts <1239605708@qq.com>
2021-01-14 00:58:10 +0800

    无改动

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,14 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [{
-  path: '/',
-  name: 'index',
-  component: () => import('../views/index.vue')
+    path: '/',
+    name: 'index',
+    component: () =>
+        import ('../views/index.vue')
 }]
 const router = new VueRouter({
-  mode: "history",
-  routes
+    mode: "history",
+    routes
 })
 
 export default router
