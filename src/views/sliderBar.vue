c327c2e52a1f59c0b2d9f44ff9684694bb9ae409
FRDlesmsts <1239605708@qq.com>
2021-01-17 21:27:34 +0800

    Revert "Revert "left变化""
    This reverts commit 9ac96482dc9f66e207ee5f7418afdad8a90c6d63.

--- src/views/sliderBar.vue
+++ src/views/sliderBar.vue
@@ -124,6 +124,7 @@ export default {
 .slider .block {
   width: 20px;
   height: 20px;
+  margin-left: -20px;
   border-radius: 10px;
   border: 2px solid #409eff;
   background: rgba(255, 255, 255, 1);
